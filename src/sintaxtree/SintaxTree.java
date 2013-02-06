3a2f3f7c37d4f225192dfef99943b243b67b5979
Roman <Roman@10.230.56.40>
2013-02-06 11:55:07 +0000

    final refactoring

--- src/sintaxtree/SintaxTree.java
+++ src/sintaxtree/SintaxTree.java
@@ -3,6 +3,12 @@ package sintaxtree;
 public class SintaxTree {
 
     public static void main(String[] args) {
+        Node tres = new ConstantNode(3);
+        Node dos = new ConstantNode(2);
+        Node multiplicacion = new MultiplicationOperatorNode(dos, tres);
+        Node uno = new ConstantNode(1);
+        Node suma = new AddOperatorNode(uno, multiplicacion);
         
+        System.out.println(suma.evaluate());
     }
 }
