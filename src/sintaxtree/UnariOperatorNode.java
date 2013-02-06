a9565ad981c71591b48536736066b910689917e7
Roman <Roman@10.230.56.40>
2013-02-06 11:57:45 +0000

    final refactoring

--- src/sintaxtree/UnariOperatorNode.java
+++ src/sintaxtree/UnariOperatorNode.java
@@ -1,5 +1,15 @@
 package sintaxtree;
 
 public abstract class UnariOperatorNode extends Node{
+    
     private Node son;
+
+    public Node getSon() {
+        return son;
+    }
+
+    public void setSon(Node son) {
+        this.son = son;
+    }
+    
 }
