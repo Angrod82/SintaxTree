6d0d7db6c0f90a70bb3b8743a8448888a3db0ad8
Roman <Roman@10.230.56.40>
2013-03-26 15:32:56 +0000

    Method lookup in signatureHashMap

--- src/Evaluator/Nodes/Operations/OperationEvaluator.java
+++ src/Evaluator/Nodes/Operations/OperationEvaluator.java
@@ -1,30 +1,19 @@
 package Evaluator.Nodes.Operations;
 
-import Evaluator.Calculator.*;
+import Evaluator.SintaxTree;
 import Evaluator.Types.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class OperationEvaluator {
     
-    private static Calculator searchCalculator(Type arg1, Type arg2) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        if(arg1.getValue() instanceof Double && arg2.getValue() instanceof Double)
-            return (Calculator)Class.forName("Evaluator.Calculator.Calculators.NumberCalculator").newInstance();
-        if(arg1.getValue() instanceof Double && arg2.getValue() instanceof Integer)
-            return (Calculator)Class.forName("Evaluator.Calculator.Calculators.NumberCalculator").newInstance();
-        if(arg1.getValue() instanceof Integer && arg2.getValue() instanceof Double)
-            return (Calculator)Class.forName("Evaluator.Calculator.Calculators.NumberCalculator").newInstance();
-        if(arg1.getValue() instanceof Integer && arg2.getValue() instanceof Integer)
-            return (Calculator)Class.forName("Evaluator.Calculator.Calculators.NumberCalculator").newInstance();
-        return null;
+    private static Method searchMethod(Type arg1, Type arg2, String operatorName) {
+        return SintaxTree.signatureHashMap.get(operatorName + arg1.getValue().getClass().getSimpleName() + arg2.getValue().getClass().getSimpleName());
     }
     
-    private static Method searchMethod(Type arg1, Type arg2, String operatorName) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SecurityException, NoSuchMethodException {
-        return searchCalculator(arg1, arg2).getClass().getMethod(operatorName, arg1.getValue().getClass(), arg2.getValue().getClass());
-    }
-    
-    public static Type execute(Type arg1, Type arg2, String operatorName) throws ClassNotFoundException, InstantiationException, SecurityException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        return Converter.castToType(searchMethod(arg1, arg2, operatorName).invoke(searchCalculator(arg1, arg2), arg1.getValue(), arg2.getValue()));
+    public static Type execute(Type arg1, Type arg2, String operatorName) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException {
+        Method method = searchMethod(arg1, arg2, operatorName);
+        return Converter.castToType(method.invoke(method.getDeclaringClass().newInstance(), arg1.getValue(), arg2.getValue()));
     }
     
 }
